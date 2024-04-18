6d7e3588cdf71d1e81ab9e4d642bc399816fbb42
sieonp <aplus5040@gmail.com>
2024-04-18 21:32:39 +0900

    main initialized

---
+++ main.py
@@ -0,0 +1,17 @@
+from typing import List
+
+def even_list(int_lsit : List[int]) -> List[int]:
+	pass
+
+def sum_of_squares_of_even(even_int_list : List[int]) -> int:
+	pass
+
+def main():
+	int_list = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+	even_int_list = even_lsit(int_list)
+	output = sum_of_squares_of_even(even_int_list)
+	print(output)
+
+if __name__ == "__main__":
+	main()
+
